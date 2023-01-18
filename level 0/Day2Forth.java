774811dfafc570fb01578f71c6f7b4b54a5ca85b
hil <hil679922@gmail.com>
2023-01-18 23:06:43 +0900

    배열 두 배 만들기.java

--- level 0/Day2Forth.java
+++ level 0/Day2Forth.java
@@ -2,8 +2,9 @@ import java.util.*;
 
 
 class Day2Forth{
-    public static void main(String[] args) {
-        System.out.println(solution(new int[]{1,2,3,4}));
+    public static void main(String[] args) {//java에서 main 항상 정적 => 정적 method만 호출 가능, 따라서 solution에도 static
+        int[] arr = {1,2,3,4};
+        System.out.println(solution(arr));//new int[]{1,2,3,4}도 가능, {1,2,3,4}는 모호해서 안 됨
     }
     public static int[] solution(int[] numbers) {
         int[] answer = {};
