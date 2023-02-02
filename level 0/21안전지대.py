1d4949fa83e68350b0c68f1648eb66bc97e672b8
hil <hil679922@gmail.com>
2023-02-02 23:19:29 +0900

    simulation

---
+++ level 0/21안전지대.py
@@ -0,0 +1,25 @@
+def solution(board):
+    N = len(board)
+    dx = [-1, 1, 0, 0, -1, -1, 1, 1]
+    dy = [0, 0, -1, 1, -1, 1, -1, 1]
+
+    booms = []
+    for x in range(N):
+        for y in range(N):
+            if board[x][y] == 1:
+                booms.append((x, y))
+
+    for x, y in booms:
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
+                board[nx][ny] = 1
+
+    count = 0
+    for x in range(N):
+        for y in range(N):
+            if board[x][y] == 0:
+                count += 1
+
+    return count
