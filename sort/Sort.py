4f484c49eca5cdc4c3e684d27263f46408d4c42d
hil679 <hil679922@gmail.com>
2024-01-03 22:35:11 +0900

    sort: 삽입 정렬

--- sort/Sort.py
+++ sort/Sort.py
@@ -10,7 +10,16 @@ class Sort:
 
         print(array)
     
-
+    def insertSort():
+        array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+        for i in range(1, len(array)):
+            for j in range(i, 0, -1):
+                if(array[j] < array[j -1]):
+                    array[j], array[j -1] = array[j - 1], array[j]
+                else:
+                    break
+        print(array)
 
 #실행
 Sort.selectionSort()
+Sort.insertSort()
