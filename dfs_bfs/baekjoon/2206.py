7290d70241120c5a92e869d294892491ef111d0e
hil679 <hil679922@gmail.com>
2024-09-12 22:19:07 +0900

    [bfs] 조건까지 queue에 넣기 -> 3차원 배열

--- dfs_bfs/baekjoon/2206.py
+++ dfs_bfs/baekjoon/2206.py
@@ -1,51 +1,39 @@
 from collections import deque
-import copy
-
-def check_wall(map_arr):
-    for x in range(len(map_arr)):
-        for y in range(len(map_arr[0])):
-            if x == 0 and y == 0:
-                continue
-            if map_arr[x][y] == 1:
-                wall_pos.append((x,y))
-
-
 n, m = map(int, input().split())
-map_arr = [list(map(int, input())) for i in range(n) ] 
-wall_pos = deque()
-check_wall(map_arr)
+map_arr = [list(map(int, input())) for i in range(n)]
+visited = [[[0,0] for y in range(len(map_arr[0]))] for x in range(len(map_arr))]
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
 que = deque()
-que.append((0,0))
+que.append((0,0,0))
 answer = -1
+visited[0][0][0] = 1
 
-while(wall_pos):
-    visited = copy.deepcopy(map_arr)
-    visited[0][0] = 1
-    wall_x, wall_y = wall_pos.popleft()
-    visited[wall_x][wall_y] = 0
-    while(que):
-        x,y=que.popleft()
-        for i in range(len(dx)):
-            next_x = x + dx[i]
-            next_y = y + dy[i]
+while(que):
+    x,y,break_wall=que.popleft()
+    for i in range(len(dx)):
+        next_x = x + dx[i]
+        next_y = y + dy[i]
 
-            if (next_x < 0 or next_y < 0 or next_x >= n or next_y >=m or visited[next_x][next_y] != 0):
-                continue
-            if (next_x == n -1 and next_y == m - 1):
-                visited[next_x][next_y] = visited[x][y] + 1
-                break
-            else:
-                visited[next_x][next_y] = visited[x][y] + 1
-                que.append((next_x, next_y))
-        if visited[n - 1][m - 1] != 0:
-            que.clear()
+        if (next_x < 0 or next_y < 0 or next_x >= n or next_y >= m or visited[next_x][next_y][break_wall]!= 0):
+            continue
+        elif (map_arr[next_x][next_y] == 0):
+            visited[next_x][next_y][break_wall] = visited[x][y][break_wall] + 1
+            que.append((next_x, next_y, break_wall))
+        elif (map_arr[next_x][next_y] == 1 and break_wall == 0):
+            visited[next_x][next_y][1] = visited[x][y][break_wall] + 1
+            que.append((next_x, next_y, 1))
+        elif (next_x == n -1 and next_y == m - 1):
+            visited[next_x][next_y][break_wall] = visited[x][y][break_wall] + 1
             break
-    if visited[n - 1][m - 1] != 0: 
-        if answer == -1 or answer > visited[n - 1][m - 1]:
-            answer = visited[n - 1][m - 1]
+if visited[n - 1][m - 1][0] != 0: 
+    if visited[n - 1][m - 1][1] != 0:
+        answer = min(visited[n - 1][m - 1][0], visited[n - 1][m - 1][1])
+    else:
+        answer = visited[n - 1][m - 1][0]
+elif visited[n - 1][m - 1][1] != 0:
+    answer = visited[n - 1][m - 1][1]
 
 print(answer)
 
