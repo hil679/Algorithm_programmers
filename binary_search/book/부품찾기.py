c187b687d9d9e92af97486fcd6727e3248a73a10
hil679 <hil679922@gmail.com>
2024-09-14 23:33:19 +0900

    [binary search] 부품찾기

---
+++ binary_search/book/부품찾기.py
@@ -0,0 +1,5 @@
+import sys
+n = sys.stdin.readline().rstrip()
+all_toop = map(int, sys.stdin.readline().rstrip().split())
+m = sys.stdin.readline().rstrip()
+buy_tool = map(int, sys.stdin.readline().rstrip().split()) 
